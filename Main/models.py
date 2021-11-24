dd873daa9e2f4ee4a1a72f6af28e6010ce8af396
Germán Lazcano <german.lazcano@alumnos.mdp.utn.edu.ar>
2021-11-24 00:21:48 -0300

    Login agregado (sin el form)

--- Main/models.py
+++ Main/models.py
@@ -1,11 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    email = models.EmailField()
+class MyUser(User):
     friends = models.ManyToManyField("self")
 
 
@@ -23,8 +20,8 @@ class Task(models.Model):
     Content = models.TextField(max_length=200)
     CreateDate = models.DateField(max_length=200)
     Status = models.ForeignKey(Label, on_delete=models.CASCADE)
-    UsersTags = models.ManyToManyField(User, related_name='users_tags')
-    Owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    UsersTags = models.ManyToManyField(MyUser, related_name='users_tags')
+    Owner = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     Category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
--- Main/views.py
+++ Main/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def login_view(request):
+    if request.method == 'GET':
+        return render(request,'Main/form.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            ...
+        else:
+            # Return an 'invalid login' error message.
+            ...
+
 from Main.models import Task, Category
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def all_tasks_by_user(request):
     tasks = Task.objects.all()
     categories = Category.objects.all()
